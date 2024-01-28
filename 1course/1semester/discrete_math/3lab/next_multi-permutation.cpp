c9adeedf851098dfbe291af724583b8e6f8ba154
Matruhovich Ilia <150400272+1lushaa@users.noreply.github.com>
2024-01-28 03:25:26 +0300

    Create next_multi-permutation.cpp

---
+++ 1course/1semester/discrete_math/3lab/next_multi-permutation.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void gen(vector<int>& a) {
+    int idx = -1;
+    for (int i = a.size() - 1; i > 0; i--) {
+        if (a[i - 1] < a[i]) {
+            idx = i - 1;
+            int min = i;
+            for (int j = i; j < a.size(); j++) {
+                if (a[j] > a[idx]) {
+                    min = j;
+                }
+                else {
+                    break;
+                }
+            }
+            swap(a[idx], a[min]);
+            break;
+        }
+    }
+    if (idx == -1) {
+        for (int i = 0; i < a.size(); i++) {
+            cout << "0 ";
+        }
+    }
+    else {
+        for (int i = 0; i <= idx; i++) {
+            cout << a[i] << " ";
+        }
+        for (int i = a.size() - 1; i > idx; i--) {
+            cout << a[i] << " ";
+        }
+    }
+    cout << endl;
+    return;
+}
+int main() {
+    int n;
+    cin >> n;
+    vector<int>a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    gen(a);
+}
