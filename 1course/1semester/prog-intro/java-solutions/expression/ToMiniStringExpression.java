8fb2fc06f76aa42348723edde3b0f92de3dadc39
Matruhovich Ilia <150400272+1lushaa@users.noreply.github.com>
2024-04-20 22:40:28 +0300

    cosmetic upod

--- 1course/1semester/prog-intro/java-solutions/expression/ToMiniStringExpression.java
+++ 1course/1semester/prog-intro/java-solutions/expression/ToMiniStringExpression.java
@@ -1,6 +1,5 @@
 package expression;
 
 public interface ToMiniStringExpression extends Expression, TripleExpression, ToMiniString {
-    // Note:: not needed for user
     void toString(StringBuilder sb);
 }
